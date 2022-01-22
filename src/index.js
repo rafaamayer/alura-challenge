35868fd05b17f956a70d2e4e3f4f6212e70c1063
Rafael Mayer <rafa.m.mayer@hotmail.com>
2022-01-21 22:57:43 -0300

    feature: routes implemented

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
+import Comunidade from './routes/Comunidade'
 import reportWebVitals from './reportWebVitals';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={ <App /> } />
+      <Route path="comunidade" element={ <Comunidade /> } />
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
