b8dbb7a822db5746f2202309facb5e12a2c3ac84
Rafael Mayer <rafa.m.mayer@hotmail.com>
2022-02-09 16:44:07 -0300

    feature: trying via element hljs

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -7,9 +7,9 @@ export function Button(props) {
 
     function highlightCode(){
         const textArea = document.querySelector('textarea.code')
-        const code = textArea.value
-        console.log(code)
-        hljs.highlight(code, {language: 'javascript'})
+        //const code = textArea.value
+        hljs.highlightElement(textArea) //Maybe this is the way
+       // hljs.highlight(code, {language: 'javascript'})
     }
 
     return (
